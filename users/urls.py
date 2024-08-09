0e254ebd05d1e97e1e002cab159c6b5873b91e2b
thays12sil <taina.silva@escolar.ifrn.edu.br>
2024-08-08 22:25:42 -0300

    add taina

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import luiz
+from .views import luiz, taina
+
+
 
 urlpatterns = [
     path("luiz", luiz, name="luiz"),
+    path("taina", taina, name="taina"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def luiz(request):
     return render(request, "luiz.html")
+
+def taina(request):
+    return render(request, "taina.html")
